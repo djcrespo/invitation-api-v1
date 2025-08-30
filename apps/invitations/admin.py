7f39b9611e3e842739099d020a9039fd71a1db91
DJCrespo <dj.crespo.castilla@gmail.com>
2025-08-30 11:36:35 -0600

    fix bug in admin

--- apps/invitations/admin.py
+++ apps/invitations/admin.py
@@ -5,7 +5,6 @@ from .models import Invitation
 class AdminInvitation(admin.ModelAdmin):
     ordering = ('-created_at',)
     list_display = ('id', 'confirm', 'from_person', 'group_person', 'type', 'created_at', 'updated_at')
-    search_fields = ('id', )
-    filter_vertical = ('confirm',)
-    filter_horizontal = ('persons',)
+    search_fields = ('id', 'persons__full_name', )
+    list_filter = ('confirm', 'from_person', 'group_person', 'type')
     readonly_fields = ('created_at', 'updated_at')
