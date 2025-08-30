931f5f2481e56fb74f613568bfcb87546c6d74a4
DJCrespo <dj.crespo.castilla@gmail.com>
2025-08-30 11:34:09 -0600

    update admin

--- apps/invitations/admin.py
+++ apps/invitations/admin.py
@@ -4,8 +4,8 @@ from .models import Invitation
 @admin.register(Invitation)
 class AdminInvitation(admin.ModelAdmin):
     ordering = ('-created_at',)
-    list_display = ('id', 'confirm', 'from_person', 'group_person', 'type', 'persons', 'created_at', 'updated_at')
-    search_fields = ('id', 'persons__full_name', )
+    list_display = ('id', 'confirm', 'from_person', 'group_person', 'type', 'created_at', 'updated_at')
+    search_fields = ('id', )
     filter_vertical = ('confirm',)
     filter_horizontal = ('persons',)
     readonly_fields = ('created_at', 'updated_at')
